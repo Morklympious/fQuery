28bb75aa06d8dfe8421bc916eb0e9262ebf0bad4
Bradley Stafford <morklympious@gmail.com>
2016-11-11 09:27:47 -0800

    Add jsdoc for DOM module.

--- src/js/dom.js
+++ src/js/dom.js
@@ -3,11 +3,28 @@ var helpers   = require("./internal/helpers"),
     _exclude  = helpers.exclude,
     _unwrap   = helpers.unwrap;
 
-// Find a nested element
+/**
+ * Finds an element by selector within a context. If child elements in 
+ * element match selector, they are added to the return result
+ * 
+ * @param {Node} element - The element to search for children inside of
+ * @param {string} selector - The CSS selector to match elements against
+ * 
+ * @returns {array|Node} A single element or collection of elements that match the query
+ */
 function find(element, selector) {
   return _unwrap(_elements(element.querySelectorAll(selector)));
 }
 
+/**
+ * Finds an element by selector within the context of context (document.body 
+ * if no context is supplied). This is a bare-bones analog of jQuery's `$()`
+ * 
+ * @param {string} selector - The CSS selector to match elements against
+ * @param {Node} context - The element to search inside of
+ * 
+ * @returns {array|Node} A single element or collection of elements that match the query
+ */
 function query(selector, context) {
   context = context || document.body; 
 
@@ -15,6 +32,15 @@ function query(selector, context) {
 }
 
 // find the closest element that matches (includes self)
+/**
+ * Finds the closest parent element of element that matches selector
+ * starting with the element this function was passed
+ * 
+ * @param {Node} element - The element from which to begin the search
+ * @param {string} selector - The CSS selector to match elements against
+ * 
+ * @returns {Node} The first ancestor element that matches the query
+ */
 function closest(element, selector) {
   var current  = element;
 
@@ -27,24 +53,43 @@ function closest(element, selector) {
   return (current.matches(selector)) ? current : [];
 }
 
-// Return node parent
+/**
+ * Returns the immediate parent of a given node
+ * 
+ * @param {Node} element - The element from which to return a parent
+ * 
+ * @return {Node} element - The parent element of the element passed in
+ */
 function parent(element) {
   return element.parentNode;
 }
 
-// Get immediate children
+/**
+ * Returns an array of children of the passed in element. Will return a single element
+ * if the array contains only one child. 
+ * 
+ * @param {Node} element - The element from which to retrieve children
+ * 
+ * @return {array|Node} element - The children of element
+ */
 function children(element) {
   return _unwrap(_elements(element.childNodes));
 }
 
+/**
+ * Returns siblings of a given node. If a selector is specified, it will
+ * return only the siblings that match that selector. 
+ * 
+ * @param {Node} element - The element from which to return a parent
+ * 
+ * @return {array|Node} element - The sibling(s) of the element passed in. 
+ */
 function siblings(element, selector) {
   var result = selector ? _exclude(find(parent(element), selector), element) :
                           _exclude(children(parent(element)), element);
   
   return _unwrap(result);
 }
-
-// Possible additions: .next(), .previous(). 
 
 module.exports = {
   query    : query, 
